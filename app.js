f6e157c198a7a1676df372e2239ed8a8cba6c2aa
Sarthak Negi <sarthak@illuminz.com>
2019-12-22 15:28:42 +0530

    Next comm

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
 res.writeHead(200);
-res.write("Hello there");
+res.write("Hello there you beauty");
 res.end();
 }).listen(3000, () => { console.log('Listening on 3000'); });
 
